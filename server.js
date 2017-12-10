8a5b39e5871e3c54ec3e4c7d35d947b295ff75cf
Ernesto Hernandez <h_ernesto@ymail.com>
2017-12-10 14:11:33 -0800

    Folders Laid Out and some modules have been required

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(express.static("public"));
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
